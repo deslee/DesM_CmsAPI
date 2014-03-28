19b8108faf46ce733cf0c755727172ea29189aee
desmond <desmondclee@gmail.com>
2014-03-27 23:02:00 -0500

    tabs

--- server/configs/secret.js
+++ server/configs/secret.js
@@ -1,5 +1,5 @@
 // this key is used to sign the authentication tokens.
-module.exports.key 		= "You should change this";
+module.exports.key	= "You should change this";
 
-module.exports.username = "admin";	// login for sample user
-module.exports.password = "pass";	// password for sample admin
+module.exports.username	= "admin";	// login for sample user
+module.exports.password	= "pass";	// password for sample admin
